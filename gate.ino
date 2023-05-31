acd76cd7f58da4867d3cec3e16dfac2dd925a8b8
MatheusBonadio <matheus_lima_bonadio_xd@hotmail.com>
2023-05-31 17:12:44 -0300

    Add unixtime

--- gate.ino
+++ gate.ino
@@ -2,21 +2,34 @@
 #include <arduino_base64.hpp>
 #include <SoftwareSerial.h>
 #include <EEPROM.h>
+#include <RTClib.h>
 
 #define ARDUINO_MEMORY 1024
+#define CODE_LENGTH 32
+
+SoftwareSerial HC12(2, 3);
+RTC_DS1307 rtc;
 
 extern uint8_t key[];
-SoftwareSerial HC12(2, 3);
 int sequence = 0;
-long seed;
+unsigned long seed;
 
 void setup() {
   Serial.begin(9600);
   HC12.begin(9600);
+
+  if (!rtc.begin()) {
+    Serial.println("DS1307 não encontrado");
+    while (true) {}
+  }
+
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
 void loop() {
+
   if (HC12.available()) {
+    DateTime now = rtc.now();
     String code = HC12.readStringUntil("\n");
 
     code = decryptCode(code);
@@ -24,7 +37,7 @@ void loop() {
     Serial.print("Descriptografado: ");
     Serial.println(code);
 
-    long splittedCode[2];
+    unsigned long splittedCode[3];
     splitCode(code, splittedCode);
 
     if (!checkSeed(splittedCode[1])) {
@@ -36,6 +49,8 @@ void loop() {
 
       if (i > sequence) {
         sequence = i;
+        Serial.print("Tempo de leitura (s): ");
+        Serial.println(now.unixtime() - splittedCode[2]);
         switchGate();
       } else {
         Serial.println("Código fora da sequência.");
@@ -69,7 +84,7 @@ long readMemory(int address) {
 }
 
 // Write in memory from EEPROM in 2 addresses
-void writeMemory(int address, long seed) {
+void writeMemory(int address, unsigned long seed) {
   byte hb = highByte(seed);
   byte lb = lowByte(seed);
 
@@ -100,8 +115,8 @@ int searchEmptyIndexInMemory() {
   return index;
 }
 
-// Splits the code into sequence (index 0 of splittedCode) and seed(index 1 of splittedCode)
-void splitCode(String decrypted_code, long *splittedCode) {
+// Splits the code into sequence(index 0 of splittedCode), seed(index 1 of splittedCode) and unixtime(index 2 of splittedCode)
+void splitCode(String decrypted_code, unsigned long *splittedCode) {
 
   char *code_char = decrypted_code.c_str();
   code_char = strtok(code_char, " ");
@@ -114,10 +129,14 @@ void splitCode(String decrypted_code, long *splittedCode) {
   code_char = strtok(NULL, "-");
 
   splittedCode[1] = atol(code_char);
+
+  code_char = strtok(NULL, "-");
+
+  splittedCode[2] = atol(code_char);
 }
 
 // Store seed in EEPROM memory in cycles preceded by value 1
-void storeSeed(long seed) {
+void storeSeed(unsigned long seed) {
   int lastIndex = searchEmptyIndexInMemory();
 
   writeMemory(lastIndex, seed);
@@ -131,7 +150,7 @@ void storeSeed(long seed) {
 }
 
 // Check if the current seed is the last one registered in the EEPROM memory
-bool checkLastSeed(long seed) {
+bool checkLastSeed(unsigned long seed) {
   int lastIndex = searchEmptyIndexInMemory();
 
   if (lastIndex == 2)
@@ -149,10 +168,10 @@ bool checkLastSeed(long seed) {
 }
 
 // Check if the seed has already been used in the EEPROM memory
-bool checkSeed(long seed) {
+bool checkSeed(unsigned long seed) {
   for (int i = 2; i < ARDUINO_MEMORY; i += 2) {
-    if (readMemory(i) == 0)
-      break;
+    // if (readMemory(i) == 0)
+    //   break;
 
     if (seed == readMemory(i)) {
       if (checkLastSeed(seed))
@@ -185,12 +204,12 @@ void switchGate() {
   }
 }
 
-// Pads with spaces until the string is 16 characters long
+// Pads with spaces until the string is mod 16 characters long
 String paddingString(String text) {
   char *text_char = text.c_str();
 
-  if (strlen(text_char) <= 16) {
-    for (int i = strlen(text_char); i < 16; i++)
+  if (strlen(text_char) <= CODE_LENGTH) {
+    for (int i = strlen(text_char); i < CODE_LENGTH; i++)
       text.concat(" ");
 
     return text;
@@ -200,13 +219,13 @@ String paddingString(String text) {
 
 // Encrypts a string with AES and base64
 String encryptCode(String code) {
-  char code_char[16];
+  char code_char[CODE_LENGTH];
 
-  for (int i = 0; i < 16; i++) {
+  for (int i = 0; i < CODE_LENGTH; i++) {
     code_char[i] = code.c_str()[i];
   }
 
-  aes128_enc_single(key, code_char);
+  aes128_enc_multiple(key, code_char, sizeof(code_char));
 
   auto code_length = sizeof(code_char);
   char encrypted_code[base64::encodeLength(code_length)];
@@ -222,7 +241,7 @@ String decryptCode(String code) {
   uint8_t decrypted_code[base64::decodeLength(code_char)];
   base64::decode(code_char, decrypted_code);
 
-  aes128_dec_single(key, decrypted_code);
+  aes128_dec_multiple(key, decrypted_code, sizeof(decrypted_code));
 
   return decrypted_code;
 }
